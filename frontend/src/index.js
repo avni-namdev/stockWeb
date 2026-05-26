30bad28f10904501d97f3a3ac1ffcd4e31b2c161
AVNI NAMDEV <namdevavni088@gmail.com>
2026-05-26 14:08:12 +0530

    installed axios in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import HomePage from "./landing_page/home/HomePage";
+
 import Signup from "./landing_page/signup/Signup";
+
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
